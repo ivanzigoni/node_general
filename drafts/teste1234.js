e3720e58a0e30dbfbacd4a0d84efbad2ada4331d
ivan <ivan@pop-os.localdomain>
2023-01-26 14:25:46 -0300

    feat: addss file guard before ending strem

---
+++ drafts/teste1234.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+fs.rm("/home/ivan/Documents/testes/node_general/files/image.jpg", (err) => {
+    console.log("batata")
+});
--- server001/functions/teste3/index.js
+++ server001/functions/teste3/index.js
@@ -2,15 +2,38 @@ const gm = require("gm").subClass({ appPath: String.raw`/home/ivan/Downloads/mag
 const fs = require("fs");
 const randomHex = require('random-hex');
 
-function main(res, options) {
+function generateImage(path) {
+  gm(400, 400, randomHex.generate())
+  .drawText(200, 200, Array.from({ length: 200 }).reduce(acc => acc + `k`, `j`), "wat")
+  .magnify(0.1)
+  .write(path, function (err) {
+    if (err) {
+      console.log(err, "sopinha pa nois");
+    }
+  })
+}
 
-    gm(400, 400, randomHex.generate())
-    .drawText(10, 50, "from scratch")
-    .write("/home/ivan/Documents/testes/node_general/files/teste1.jpg", function (err) {
-      console.log(err)
-    });
+async function main(res, options) {
 
+    const path = "/home/ivan/Documents/testes/node_general/files/teste1.jpg";
 
+    generateImage(path);
+
+    while (!fs.existsSync(path)) {
+      console.log("waiting for file")
+    }
+
+    const stream = fs.createReadStream(path)
+      .on("data", (chunk) => { res.write(chunk); })
+      .on("end", () => {
+        fs.rm(path, (err) => { if (err) console.log(err, "frango com batata"); });
+        stream.close((err) => { if (err) console.log(err); })
+        res.end();
+      })
+      .on("error", (err) => {
+        console.log(err);
+        res.end("error reading stream")
+      })
 }
 
 function teste3(res, options) {
